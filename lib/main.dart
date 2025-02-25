62efcddd03d155ac1d9589cab39f91045f013b7f
elifibrahimbas1 <elifn.ibrahimbas@gmail.com>
2025-02-25 20:44:13 +0300

    ilk yorum

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
+              '$_counter'
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
@@ -120,3 +120,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+//elif
