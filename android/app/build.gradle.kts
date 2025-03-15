0b1b677638de50e4c0e2c34d5a1ba7fbd808d50e
BERA <116309923+beracvk@users.noreply.github.com>
2025-03-15 12:21:19 +0300

    gradle config.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
     id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -12,11 +9,17 @@ android {
     ndkVersion = "27.0.12077973"
     namespace = "com.example.fitopia2"
     compileSdk = flutter.compileSdkVersion
-    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    sourceSets {
+        // Bu bloğu doğru bir şekilde yapılandırıyoruz.
+        getByName("main") {
+            manifest.srcFile("android/app/src/main/AndroidManifest.xml")  // Manifest dosyasına doğru referans
+        }
     }
 
     kotlinOptions {
@@ -24,10 +27,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.fitopiaCrew"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -36,8 +36,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
