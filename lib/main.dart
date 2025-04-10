4cffbc56779b8bbabdc91d2a75146753956e80ee
edaltuntas23 <edaltuntas23@gmail.com>
2025-04-10 16:59:24 +0300

    akşam yemeği sayfa

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-// MyApp burada tanımlı mı?
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitopia2/screens/firebase_home_screen.dart'; // Firebase'den veri çeken ekranı içe aktar
+import 'firebase_options.dart'; // Firebase yapılandırma dosyası
 
-void main() {
-  runApp(MyApp()); // Fonksiyon gereksiz, doğrudan çağır
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter başlatılmadan önce işlemleri yap
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Firebase yapılandırması
+  );
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,11 +18,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Fitopia")),
-        body: Center(child: Text("Hoş geldiniz!")),
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home:
+          const FirebaseHomeScreen(), //  Firebase'den veri çeken ekran başlangıcı
     );
   }
 }
-
