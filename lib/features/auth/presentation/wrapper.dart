796d75161edba57c056fac87d66bed387a300493
BERA <116309923+beracvk@users.noreply.github.com>
2025-04-23 22:12:48 +0300

    wrapper config.

--- lib/features/auth/presentation/wrapper.dart
+++ lib/features/auth/presentation/wrapper.dart
@@ -2,11 +2,11 @@
 
 import 'package:fitopia2/features/auth/presentation/pages/Sixth_screen.dart';
 import 'package:fitopia2/shared/firebase_home_screen.dart';
-import 'package:fitopia2/features/home/presentation/pages/home3_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../../../screens/screens/home_screen.dart'; // Giriş ekranın burada olsun
 import 'package:fitopia2/features/onboarding/presentation/pages/Third_screen.dart';
+import 'package:fitopia2/features/onboarding/presentation/pages/home_screen.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
@@ -23,7 +23,7 @@ class Wrapper extends StatelessWidget {
         } else if (snapshot.hasData) {
           return const FirebaseHomeScreen(); // Kullanıcı oturum açmışsa ana ekran
         } else {
-          return const ThirdScreen(); // Kullanıcı oturum açmamışsa giriş ekranı
+          return const HomeScreen(); // Kullanıcı oturum açmamışsa giriş ekranı
         }
       },
     );
