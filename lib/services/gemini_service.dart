547db732b6fead3e5ecc8a3c7adf9d859b876bff
elifibrahimbas1 <elifn.ibrahimbas@gmail.com>
2025-05-16 15:25:41 +0300

    gemini service dart

---
+++ lib/services/gemini_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class GeminiService {
+  final String apiKey = 'BURAYA_API_KEYİNİ_YAPIŞTIR';
+
+  Future<String> generateText(String prompt) async {
+    final url = Uri.parse(
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$apiKey',
+    );
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        "contents": [
+          {
+            "parts": [
+              {"text": prompt},
+            ],
+          },
+        ],
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final text = data['candidates'][0]['content']['parts'][0]['text'];
+      return text;
+    } else {
+      return 'Hata: ${response.statusCode}';
+    }
+  }
+}
